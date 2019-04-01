69df1e0eef2505c1399cbd1bf38a1dc841e66fdf
Jon <jon.ledbetter@gmail.com>
2019-04-01 19:52:02 -0400

    game finished, but visually sparse

---
+++ assets/javascript/game.js
@@ -0,0 +1,103 @@
+
+var wins = 0;
+var losses = 0;
+var numArray = [];
+
+startGame();
+
+function startGame() {
+    // clear last round:
+    $("#crystals").text('');
+    $("#win-or-loss").text('');
+    $("#current-score").text(currentScore);
+
+    // Random target number (between 19 and 120)
+    var targetNumber = (Math.round(Math.random() * 101));
+    console.log("target: " + targetNumber);
+    targetNumber += 19;
+    console.log("target: " + targetNumber);
+
+    var currentScore = 0;
+    // var numArray = [1, 10, 15, 20];
+    numArray = randomValues();
+    console.log(numArray);
+
+
+    $("#number-to-guess").text(targetNumber);
+    $("#current-score").text(currentScore);
+
+    // initialize crystals:
+    for (var i = 0; i < numArray.length; i++) {
+
+        var thisCrystal = $("<img>");
+        thisCrystal.addClass("crystal-image");
+
+        // apply respective images:
+        if (i === 0) thisCrystal.attr("src", "assets/images/blue.jpg");
+        else if (i === 1) thisCrystal.attr("src", "assets/images/red.jpg");
+        else if (i === 2) thisCrystal.attr("src", "assets/images/white.jpg");
+        else if (i === 3) thisCrystal.attr("src", "assets/images/purple.jpg");
+
+        // apply respective numerical values:
+        thisCrystal.attr("data-crystalvalue", numArray[i]);
+
+        // add crystal to crystal container:
+        $("#crystals").append(thisCrystal);
+    }
+
+    // onClick for Crystal class
+    $(".crystal-image").on("click", function () {
+
+        var crystalValue = ($(this).attr("data-crystalvalue"));
+        crystalValue = parseInt(crystalValue);
+
+        // tally score:
+        currentScore += crystalValue;
+        $("#current-score").text(currentScore);
+
+        // win/lose conditions:
+        if (currentScore == targetNumber) {
+            $("#win-or-loss").text("You win! Restarting!");
+            wins++; $("#winsNow").text("Wins: " + wins);
+            setTimeout(function () {
+                startGame();
+            }, 3000);
+        }
+        else if (currentScore > targetNumber) {
+            $("#win-or-loss").text(currentScore + " exceeds " + targetNumber + "! You have lost! Restarting!");
+            losses++; $("#lossesNow").text("Losses: " + losses);
+            setTimeout(function () {
+                startGame();
+            }, 3000);
+        }
+    });
+}
+
+function randomValues() {
+    var tmpArray = [];
+    var curNum = 0
+    var lastNum = 0
+    var numIndex = "0"
+
+    
+    
+    for (let i = 0; i < 4; i++) {
+
+        curNum = Math.round(Math.random() * 11) + 1;
+        if (curNum == 0) curNum++;
+
+        numIndex = tmpArray.indexOf(curNum);
+
+        if (numIndex == (-1)) {
+            console.log(numIndex + " not used!");
+            tmpArray[i] = curNum;
+        } else if (numIndex != (-1)) {
+            console.log(numIndex + " used before!");
+            i--;  
+        }
+
+        console.log("i: " + i + " lastNum: " + lastNum + " curNum: " + curNum + " indexOf: " + tmpArray.indexOf(curNum) + " tmpArray: " + tmpArray);
+    }
+
+    return tmpArray;
+}
